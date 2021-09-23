ffb2921bf0729bb123b95f58f3b69245c0ba1eea
Athichart Tangpong <victorybattleship@gmail.com>
2021-09-22 23:51:07 -0700

    Add huffmanDecode

--- tree/huffman_encode.py
+++ tree/huffman_encode.py
@@ -10,13 +10,41 @@ def getWordFrequency( string ):
    return dict( freq )
 
 def huffmanEncode( string ):
+   '''
+   Encode string using Huffman's algorithm
+   '''
    freq = getWordFrequency( string )
    coding = huffmanTree( freq )
 
    answer = ''
    for s in string:
       answer += coding[ s ]
+
+   assert( string == huffmanDecode( answer, coding ) )
    return answer
+
+def huffmanDecode( string, coding ):
+   '''
+   Decode string using Huffman's algorithm
+   '''
+   reverseCoding = {}
+   for k, v in coding.items():
+      reverseCoding[ v ] = k
+
+   decoded = ''
+   while len( string ) != 0:
+      found = False
+      for code, symbol in reverseCoding.items():
+         if string.startswith( code ):
+            decoded += symbol
+            size = len( code )
+            string = string[ size: ]
+            found = True
+
+      if not found:
+         return ''
+
+   return decoded
 
 if __name__ == '__main__':
    testCases = [
@@ -25,7 +53,8 @@ if __name__ == '__main__':
             # The input has 47 characters, each of which takes one byte. So the input
             # takes 47 * 8 = 376 bits.
             'string' : 'Huffman coding is a data compression algorithm.',
-            # The encoded string takes 194 bits.
+            # The encoded string takes 194 bits which is 51.6% of the original
+            # string.
             'answer' : 194,
          }
    ]
