adaef4a59a204c3b7dd30249b522acacbd33d5ac
Athichart Tangpong <victorybattleship@gmail.com>
2022-05-28 16:57:48 -0700

    Add ex11.py

--- grokking_coding_interview/14_top_k_elements/ex11.py
+++ grokking_coding_interview/14_top_k_elements/ex11.py
@@ -2,6 +2,31 @@
 # Given a string, find if its letters can be rearranged in such a way that no two
 # same characters come next to each other.
 
+from collections import defaultdict
+from heapq import *
+
+def arrange( arr ):
+   hashMap = defaultdict( int )
+   for a in arr:
+      hashMap[ a ] += 1
+
+   maxHeap = []
+   for ch, freq in hashMap.items():
+      heappush( maxHeap, ( -freq, ch ) )
+
+   result = ''
+   while maxHeap:
+      freq, ch = heappop( maxHeap )
+      if len( result ) > 0 and result[ -1 ] == ch:
+         return ''
+      result += ch
+
+      freq += 1
+      if freq < 0:
+         heappush( maxHeap, ( freq, ch ) )
+
+   return result
+
 testCases = [
       {
          'input' : "aappp",
@@ -16,3 +41,7 @@ testCases = [
          'output' : "",
       },
 ]
+
+for test in testCases:
+   i = test[ 'input' ]
+   print( arrange( i ) )
