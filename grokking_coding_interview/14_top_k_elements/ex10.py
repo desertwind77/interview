a82274182ec445cee1375a07efed646069accf3b
Athichart Tangpong <victorybattleship@gmail.com>
2022-05-28 15:18:28 -0700

    Add ex10.py

--- grokking_coding_interview/14_top_k_elements/ex10.py
+++ grokking_coding_interview/14_top_k_elements/ex10.py
@@ -2,6 +2,29 @@
 # Given an array, find the sum of all numbers between the K1’th and K2’th smallest
 # elements of that array.
 
+from heapq import *
+
+def sumBetween( arr, k1, k2 ):
+   if k1 > k2:
+      k1, k2 = k2, k1
+   k1 = k1 + 1
+   k2 = k2 - 1
+
+   maxHeap = []
+
+   for n in arr:
+      heappush( maxHeap, -n )
+      if len( maxHeap ) > k2:
+         heappop( maxHeap )
+
+   total = 0
+   for _ in range( k2 - k1 + 1 ):
+      top = maxHeap[ 0 ]
+      heappop( maxHeap )
+      total += -top
+
+   return total
+
 testCases = [
       {
          'input' : [1, 3, 12, 5, 15, 11],
@@ -16,3 +39,10 @@ testCases = [
          'output' : 12,
       },
 ]
+
+for test in testCases:
+   i = test[ 'input' ]
+   k1 = test[ 'K1' ]
+   k2 = test[ 'K2' ]
+   o = test[ 'output' ]
+   assert( sumBetween( i, k1, k2 ) == o )
