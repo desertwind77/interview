88a819fb2990c85c5f7d60c08ebc434b27ae6abb
Athichart Tangpong <victorybattleship@gmail.com>
2022-05-29 00:54:51 -0700

    Add ex13.py (doesn't work)

--- grokking_coding_interview/14_top_k_elements/ex13.py
+++ grokking_coding_interview/14_top_k_elements/ex13.py
@@ -7,17 +7,72 @@
 #
 # If at any time the server can’t execute any task then it must stay idle.
 
+from collections import defaultdict, deque
+from heapq import *
+
+def schedule( arr, k ):
+   hashMap = defaultdict( int )
+   for a in arr:
+      hashMap[ a ] += 1
+
+   maxHeap = []
+   for ch, freq in hashMap.items():
+      heappush( maxHeap, ( -freq, ch ) )
+
+   queue = deque()
+   cpu = 0
+   sch = ''
+   while maxHeap or queue:
+      ch, freq = None, 0
+      if maxHeap:
+         freq, ch = heappop( maxHeap )
+         cpu += 1
+         sch += ch
+
+      if queue:
+         top = queue.popleft()
+         if top:
+            prevCh, prevFreq = top
+            heappush( maxHeap, ( -prevFreq, prevCh ) )
+         elif not ch:
+            cpu += 1
+            sch += 'N'
+
+      if ch:
+         freq = -freq
+         freq -= 1
+         if freq > 0:
+            for _ in range( k - len( queue ) - 2 ):
+               queue.append( None )
+            queue.append( ( ch, freq ) )
+   print( sch )
+
+   return cpu
+
 testCases = [
       # a -> c -> b -> a -> c -> idle -> a
+      # a:3,c:2,b:1     a        [None,a:2]        c:2,b:1
+      # c:2,b:1         ac       [a:2,c:1]         b:1
+      # b:1             acb      [c:1]             a:2
+      # a:2             acba     [N,a:1]           c:1
+      # c:1             acbacN   [a:1]
+      # {}              acbacN   []                a:1
+      # a:1             acbacNa  []                {}
       {
-         'input' : [a, a, a, b, c, c],
+         'input' : [ 'a', 'a', 'a', 'b', 'c', 'c' ],
          'K' : 2,
          'output' : 7,
       },
       # a -> b -> idle -> idle -> a
       {
-         'input' : [a, b, a],
+         'input' : [ 'a', 'b', 'a' ],
          'K' : 3,
          'output' : 5,
       },
 ]
+
+for test in testCases:
+   i = test[ 'input' ]
+   k = test[ 'K' ]
+   o = test[ 'output' ]
+   print( schedule( i, k ) )
