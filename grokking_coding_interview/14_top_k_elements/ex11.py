61d7a28959385dc835069fb10c542c1c1ed8e001
Athichart Tangpong <victorybattleship@gmail.com>
2022-05-28 17:30:22 -0700

    Correct ex11.py

--- grokking_coding_interview/14_top_k_elements/ex11.py
+++ grokking_coding_interview/14_top_k_elements/ex11.py
@@ -5,7 +5,7 @@
 from collections import defaultdict
 from heapq import *
 
-def arrange( arr ):
+def arrangeNotWorking( arr ):
    hashMap = defaultdict( int )
    for a in arr:
       hashMap[ a ] += 1
@@ -21,11 +21,43 @@ def arrange( arr ):
          return ''
       result += ch
 
+      # This is incorrect cause the same character may be next
+      # to each other
       freq += 1
       if freq < 0:
          heappush( maxHeap, ( freq, ch ) )
 
    return result
+
+def arrange( arr ):
+   hashMap = defaultdict( int )
+   for a in arr:
+      hashMap[ a ] += 1
+
+   maxHeap = []
+   for ch, freq in hashMap.items():
+      heappush( maxHeap, ( -freq, ch ) )
+
+   # maxHeap   result   prevChar,prevFreq
+   # a:3,p:1   ''
+   # p:1       a        (a,2)
+   # a:2       ap
+   # {}        apa      (a,1)
+   result = ''
+   prevChar, prevFreq = None, 0
+   while maxHeap:
+      freq, ch = heappop( maxHeap )
+      freq = -freq
+
+      if prevChar and prevFreq > 0:
+         heappush( maxHeap, ( -prevFreq, prevChar ) )
+
+      result += ch
+
+      prevChar = ch
+      prevFreq = freq - 1
+
+   return result if len( result ) == len( arr ) else ''
 
 testCases = [
       {
