ac6d42341c260502fb9a9df162b36475f62f2a82
Athichart Tangpong <victorybattleship@gmail.com>
2022-05-29 02:04:15 -0700

    Fixed ex12.py

--- grokking_coding_interview/14_top_k_elements/ex12.py
+++ grokking_coding_interview/14_top_k_elements/ex12.py
@@ -5,7 +5,15 @@
 from collections import defaultdict, deque
 from heapq import *
 
-def arrange( arr, k ):
+def arrange1( arr, k ):
+   # 'input' : "Programming",
+   # g:2,r:2,m:2,p:1,o:1,a:1,i:1,n:1   ''       []
+   # g:2,r:2,m:2,p:1,o:1,a:1,i:1,n:1   'g'      [None,g:1]     r:2,m:2,p:1,o:1,a:1,i:1,n:1
+   # r:2,m:2,p:1,o:1,a:1,i:1,n:1       'gr'     [g:1,r:1]      m:2,p:1,o:1,a:1,i:1,n:1
+   # m:2,p:1,o:1,a:1,i:1,n:1           'grm'    [r:1,m:1]      g:1,p:1,o:1,a:1,i:1,n:1
+   # g:1,p:1,o:1,a:1,i:1,n:1           'grmg'   [m:1]          r:1,p:1,o:1,a:1,i:1,n:1
+   # r:1,p:1,o:1,a:1,i:1,n:1           'grmgr'  []             m:1,p:1,o:1,a:1,i:1,n:1
+   # ..
    hashMap = defaultdict( int )
    for a in arr:
       hashMap[ a ] += 1
@@ -36,19 +44,40 @@ def arrange( arr, k ):
 
    return ''.join( result ) if len( result ) == len( arr ) else ''
 
+def arrange2( arr, k ):
+   hashMap = defaultdict( int )
+   for a in arr:
+      hashMap[ a ] += 1
+
+   maxHeap = []
+   for ch, freq in hashMap.items():
+      heappush( maxHeap, ( -freq, ch ) )
+
+   queue = deque()
+   result = []
+   while maxHeap:
+      negFreq, ch = heappop( maxHeap )
+      if negFreq < 0:
+         result.append( ch )
+         queue.append( ( negFreq + 1, ch ) )
+
+      # k = 2
+      # m:2,p:2   m     p:2      [m:1]          p:2
+      # p:2       mp    {}       [m:1,p:1]      m:1
+      # m:1       mpm   {}       [p:1,m:0]      p:1
+      # p:1       mpmp  {}       [m:0,p:0]      {}
+      if len( queue ) == k:
+         prevNegFreq, prevCh = queue.popleft()
+         heappush( maxHeap, ( prevNegFreq, prevCh ) )
+
+   return ''.join( result ) if len( result ) == len( arr ) else ''
+
 testCases = [
       {
          'input' : "mmpp",
          'K' : 2,
          'output' : [ "mpmp", "pmpm" ]
       },
-      # g:2,r:2,m:2,p:1,o:1,a:1,i:1,n:1   ''       []
-      # g:2,r:2,m:2,p:1,o:1,a:1,i:1,n:1   'g'      [None,g:1]     r:2,m:2,p:1,o:1,a:1,i:1,n:1
-      # r:2,m:2,p:1,o:1,a:1,i:1,n:1       'gr'     [g:1,r:1]      m:2,p:1,o:1,a:1,i:1,n:1
-      # m:2,p:1,o:1,a:1,i:1,n:1           'grm'    [r:1,m:1]      g:1,p:1,o:1,a:1,i:1,n:1
-      # g:1,p:1,o:1,a:1,i:1,n:1           'grmg'   [m:1]          r:1,p:1,o:1,a:1,i:1,n:1
-      # r:1,p:1,o:1,a:1,i:1,n:1           'grmgr'  []             m:1,p:1,o:1,a:1,i:1,n:1
-      # ..
       {
          'input' : "Programming",
          'K' : 3,
@@ -70,4 +99,5 @@ for test in testCases:
    i = test[ 'input' ]
    k = test[ 'K' ]
    o = test[ 'output' ]
-   print( arrange( i, k ) )
+   print( arrange1( i, k ) )
+   print( arrange2( i, k ) )
