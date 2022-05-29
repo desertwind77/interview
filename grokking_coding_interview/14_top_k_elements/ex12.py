db49cc186452ea76e4edb375f10c0ecdddb6ff02
Athichart Tangpong <victorybattleship@gmail.com>
2022-05-28 23:22:54 -0700

    Add ex12.py

--- grokking_coding_interview/14_top_k_elements/ex12.py
+++ grokking_coding_interview/14_top_k_elements/ex12.py
@@ -2,12 +2,53 @@
 # Given a string and a number ‘K’, find if the string can be rearranged such that the
 # same characters are at least ‘K’ distance apart from each other.
 
+from collections import defaultdict, deque
+from heapq import *
+
+def arrange( arr, k ):
+   hashMap = defaultdict( int )
+   for a in arr:
+      hashMap[ a ] += 1
+
+   maxHeap = []
+   for ch, freq in hashMap.items():
+      heappush( maxHeap, ( -freq, ch ) )
+
+   queue = deque()
+   result = []
+   while maxHeap:
+      freq, ch = heappop( maxHeap )
+      result.append( ch )
+
+      if queue:
+         top = queue.popleft()
+         if top:
+            prevCh, prevFreq = top
+            heappush( maxHeap, ( -prevFreq, prevCh ) )
+
+      freq = -freq
+      freq -= 1
+      if freq > 0:
+         curLen = len( queue )
+         for _ in range( k - curLen - 2 ):
+            queue.append( None )
+         queue.append( ( ch, freq ) )
+
+   return ''.join( result ) if len( result ) == len( arr ) else ''
+
 testCases = [
       {
          'input' : "mmpp",
          'K' : 2,
          'output' : [ "mpmp", "pmpm" ]
       },
+      # g:2,r:2,m:2,p:1,o:1,a:1,i:1,n:1   ''       []
+      # g:2,r:2,m:2,p:1,o:1,a:1,i:1,n:1   'g'      [None,g:1]     r:2,m:2,p:1,o:1,a:1,i:1,n:1
+      # r:2,m:2,p:1,o:1,a:1,i:1,n:1       'gr'     [g:1,r:1]      m:2,p:1,o:1,a:1,i:1,n:1
+      # m:2,p:1,o:1,a:1,i:1,n:1           'grm'    [r:1,m:1]      g:1,p:1,o:1,a:1,i:1,n:1
+      # g:1,p:1,o:1,a:1,i:1,n:1           'grmg'   [m:1]          r:1,p:1,o:1,a:1,i:1,n:1
+      # r:1,p:1,o:1,a:1,i:1,n:1           'grmgr'  []             m:1,p:1,o:1,a:1,i:1,n:1
+      # ..
       {
          'input' : "Programming",
          'K' : 3,
@@ -19,8 +60,14 @@ testCases = [
          'output' : "aba",
       },
       {
-         'input: "aappa",
+         'input' : "aappa",
          'K' : 3,
          'output' : "",
       },
 ]
+
+for test in testCases:
+   i = test[ 'input' ]
+   k = test[ 'K' ]
+   o = test[ 'output' ]
+   print( arrange( i, k ) )
