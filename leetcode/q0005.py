ed9ab568f77f669261bee08e3ea2655e5cfc7484
Athichart Tangpong <victorybattleship@gmail.com>
2022-05-29 01:48:28 -0700

    Add questions to leetcodes (unsolved)

---
+++ leetcode/q0005.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# Leetcode 5 : Longest Palindromic Substring
+# Given a string s, return the longest palindromic substring in s.
+
+def longestPalindrome(self, s: str) -> str:
+   pass
+
+testCases = [
+      {
+         'input' : 'babad',
+         'ouput' : 'bab',
+      },
+      {
+         'input' : 'cbbd',
+         'ouput' : 'bb',
+      },
+      {
+         'input' : 'a',
+         'ouput' : 'a',
+      },
+      {
+         'input' : 'ac',
+         'ouput' : 'a',
+      },
+]
